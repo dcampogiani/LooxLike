2f8495d6c32d6c1786d765ca1ea1f8c6d455f791
Daniele Campogiani <daniele.campogiani@gmail.com>
2015-09-04 17:26:55 +0200

    Remove onItemOptionSelected from MainActivity

--- app/src/main/java/com/disorder/looxlike/activities/MainActivity.java
+++ app/src/main/java/com/disorder/looxlike/activities/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
-import android.view.MenuItem;
 
 import com.disorder.looxlike.R;
 import com.disorder.looxlike.fragments.MainFragment;
@@ -26,14 +25,5 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
