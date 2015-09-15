39916093ea51fe2d584e3e5c01d6a36dde098a8f
Daniele Campogiani <daniele.campogiani@gmail.com>
2015-09-15 09:41:40 +0200

    HomeView: add @IntDef "as enum"

--- presentation/src/main/java/com/disorder/presentation/view/HomeView.java
+++ presentation/src/main/java/com/disorder/presentation/view/HomeView.java
@@ -1,4 +1,20 @@
 package com.disorder.presentation.view;
 
+import android.support.annotation.IntDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 public interface HomeView {
+
+    @IntDef({NEWS, USER, FAVOURITES})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface Page {
+    }
+
+    public static final int NEWS = 0;
+    public static final int USER = 1;
+    public static final int FAVOURITES = 2;
+
+    void showPage(@Page int page);
 }
