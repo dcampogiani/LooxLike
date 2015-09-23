c9a0144fcbb762b90a716b64e0a9552ed325e72e
Daniele Campogiani <daniele.campogiani@gmail.com>
2015-09-23 17:35:03 +0200

    Dagger: configuration for NewsPresenter

--- app/src/main/java/com/disorder/looxlike/application/di/components/PresentationComponent.java
+++ app/src/main/java/com/disorder/looxlike/application/di/components/PresentationComponent.java
@@ -3,7 +3,10 @@ package com.disorder.looxlike.application.di.components;
 import com.disorder.looxlike.application.di.PerActivity;
 import com.disorder.looxlike.application.di.modules.PresentationModule;
 import com.disorder.looxlike.fragments.HomeFragment;
+import com.disorder.networking.services.LooxLikeAPI;
+import com.disorder.presentation.model.mapper.NewsPostMapper;
 import com.disorder.presentation.presenter.HomePresenter;
+import com.disorder.presentation.utils.RxScheduler;
 
 import dagger.Component;
 
@@ -14,4 +17,10 @@ public interface PresentationComponent {
     void inject(HomeFragment homeFragment);
 
     HomePresenter homePresenter();
+
+    LooxLikeAPI looxLikeAPI();
+
+    RxScheduler rxScheduler();
+
+    NewsPostMapper newsPostMapper();
 }
--- app/src/main/java/com/disorder/looxlike/application/di/modules/PresentationModule.java
+++ app/src/main/java/com/disorder/looxlike/application/di/modules/PresentationModule.java
@@ -1,11 +1,19 @@
 package com.disorder.looxlike.application.di.modules;
 
 import com.disorder.looxlike.application.di.PerActivity;
+import com.disorder.networking.services.FakeLooxLikeAPI;
+import com.disorder.networking.services.LooxLikeAPI;
+import com.disorder.presentation.model.mapper.NewsPostMapper;
+import com.disorder.presentation.model.mapper.NewsPostMapperImpl;
 import com.disorder.presentation.presenter.HomePresenter;
 import com.disorder.presentation.presenter.HomePresenterImpl;
+import com.disorder.presentation.utils.MainThreadAndBackgroundRxScheduler;
+import com.disorder.presentation.utils.RxScheduler;
 
 import dagger.Module;
 import dagger.Provides;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 @Module
 public class PresentationModule {
@@ -14,5 +22,25 @@ public class PresentationModule {
     @PerActivity
     HomePresenter provideHomePresenter(HomePresenterImpl homePresenter) {
         return homePresenter;
+    }
+
+    @Provides
+    @PerActivity
+    LooxLikeAPI provideLooxLikeAPI() {
+        //TODO switch to real implementation when the communication with server is ready
+        return new FakeLooxLikeAPI();
+    }
+
+
+    @Provides
+    @PerActivity
+    RxScheduler provideRxScheduler() {
+        return new MainThreadAndBackgroundRxScheduler(AndroidSchedulers.mainThread(), Schedulers.io());
+    }
+
+    @Provides
+    @PerActivity
+    NewsPostMapper provideNewsPostMapper() {
+        return new NewsPostMapperImpl();
     }
 }
